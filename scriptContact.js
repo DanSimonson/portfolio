eb1b51290859c87ea7eb2514a95ffe80dfa83fbe
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2017-12-10 12:05:45 +0800

    redesigned contact page

--- scriptContact.js
+++ scriptContact.js
@@ -1,6 +1,15 @@
 
+   $(document).ready(function () {
+    
+      //initial check to make sure jquery loaded
+      //alert('jquery loaded');
+      
+      //force load to top
+      $(this).scrollTop(0);
+  
   //toggle the font awesome icons in a ul li
   $("#icon").click(function() {
+    //alert ('clicked button');
     $("ul.hideShow").slideToggle(2000);
     //$("ul.hideShow").toggle();
      $("ul.hideShow > li").css('display', 'inline-block');
@@ -22,7 +31,29 @@
     $("a.fa-twitter.bigIcon").removeClass('animated jello infinite');
   });
 
-  /* make navbar change on scroll */
+  window.addEventListener("scroll", yScroll);
+
+  var pagetop, menu, yPos;
+  function yScroll(){
+    pagetop = document.getElementById('pagetop');
+    menu = document.getElementById('menu');
+    yPos = window.pageYOffset;
+    if(yPos > 150){
+      pagetop.style.height = "36px";
+      pagetop.style.paddingTop = ".1px";
+      menu.style.height = "0px";
+      $("#icon").fadeIn(3000);
+
+    } else {
+      pagetop.style.height = "150px";
+      pagetop.style.paddingTop = ".1px";
+      menu.style.height = "5px";
+      $("#icon").fadeOut(3000);
+    }
+  }
+  
+
+  /* make navbar change on scroll 
     $(window).scroll(function() {
       var top = $(document).scrollTop();
 
@@ -32,3 +63,5 @@
         $('.navbar').css('background-color', 'transparent');
      }
    });
+  */
+  });
